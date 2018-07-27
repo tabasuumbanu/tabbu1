e32a973b7655f900444e90345ba8a2123db8a83a
tabasuumbanu <41725742+tabasuumbanu@users.noreply.github.com>
2018-07-27 14:12:41 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,22 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+int number;
+Scanner sc=new Scanner(System.in);
+number=sc.nextInt();
+if(number>0)
+{
+	System.out.println("positive number");
+}
+else if(number<0)
+{
+	System.out.println("negative number");
+	}
+	else
+	{
+		System.out.println("number is zero");
+ 
+}
+}
+}
